5c2190a52de8663b1365d61e9ca5129ba6171df1
Marco-Macedo <marcomacedo@ipvc.pt>
2020-12-28 16:23:20 +0000

    updates no xml

--- app/src/main/java/com/example/postnotfirebase/MainActivity.kt
+++ app/src/main/java/com/example/postnotfirebase/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.postnotfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.google.firebase.auth.FirebaseAuth
 
 
@@ -22,8 +23,16 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
         // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser = auth.currentUser
-        updateUI(currentUser)
+  /*      updateUI(currentUser)*/
     }
 
-    fun updateUI(currentUser: FirebaseUser?)
+    fun login(view: View) {
+
+    }
+
+    fun registar(view: View) {
+
+    }
+
+    /* fun updateUI(currentUser: FirebaseUser?)*/
 }
